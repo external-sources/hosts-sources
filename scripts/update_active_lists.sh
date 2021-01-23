e12c22de217173f73883424ddf9a07a24d612cb2
Spirillen <pdns@protonmail.com>
2021-01-23 19:56:50 +0100

    Hmm

--- scripts/update_active_lists.sh
+++ scripts/update_active_lists.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
+git_dir="$(git rev-parse --show-toplevel)"
+
 # This script is intended to list all active lists from the data/
 # directory for easier imports from external sources....
 # Happy havesting :)
 
-truncate -s 0 "$CI_PROJECT_DIR/sources.list"
+truncate -s 0 "${git_dir}/sources.list"
 
 for lists in `find data/ -type f -name domain.list`
 do
-	printf "$CI_PROJECT_URL/raw/master/$lists\n" | sort -u -f >> "$CI_PROJECT_DIR/sources.list"
+	printf "https://raw.githubusercontent.com/Import-External-Sources/hosts-sources/master/$lists\n" | sort -u -f >> "${git_dir}/sources.list"
 done
 
 echo -e "\n\nThe script ${0}\nExited with error code ${?}\n\n"
