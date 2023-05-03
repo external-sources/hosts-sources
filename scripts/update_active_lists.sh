ea3a1db0b6fa32cfc043706859ceb35987d7dda3
Spirillen <10725-spirillen@users.noreply.0xacab.org>
2023-05-03 22:27:21 +0000

    Update update_active_lists.sh

--- scripts/update_active_lists.sh
+++ scripts/update_active_lists.sh
@@ -10,7 +10,7 @@ truncate -s 0 "${git_dir}/sources.list"
 
 for lists in `find data/ -type f -name domain.list`
 do
-	printf "https://raw.githubusercontent.com/Import-External-Sources/hosts-sources/master/$lists\n" | sort -u -f >> "${git_dir}/sources.list"
+	printf "https://0xacab.org/my-privacy-dns/import-external-sources/hosts-sources/-/raw/master/$lists\n" | sort -u -f >> "${git_dir}/sources.list"
 done
 
 echo -e "\n\nThe script ${0}\nExited with error code ${?}\n\n"
