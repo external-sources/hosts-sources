bb6aa4710a3b26ce1c24dcc642bc74389f3a9282
Spirillen <pdns@protonmail.com>
2024-08-16 12:40:41 +0200

    Fixed an issue with url in sources 0.4
    ------
    
    Thanks to jetBrains for sponsoring IntelliJ (Ultimate Edition)
    For non-commercial open source.
    This helps My Privacy DNS to develop tools and maintain the blacklists.

--- scripts/update_active_lists.sh
+++ scripts/update_active_lists.sh
@@ -2,7 +2,7 @@
 
 git_dir="$(git rev-parse --show-toplevel)"
 
-workSpace="$(echo -n '${{ github.workspace }}')"
+url="https://github.com/external-sources/hosts-sources"
 
 # This script is intended to list all active lists from the data/
 # directory for easier imports from external sources....
@@ -13,7 +13,7 @@ truncate -s 0 "${git_dir}/sources.list"
 # shellcheck disable=SC2044
 for lists in $(find data/ -type f -name domain.list); do
     # shellcheck disable=SC2296
-    printf "$workSpace/-/raw/master/$lists\n" |
+    printf "$url/-/raw/master/$lists\n" |
         sort -u -f >>"${git_dir}/sources.list"
 done
 
